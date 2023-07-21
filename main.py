bc35ca06f7bc42e5c97b2741ee57467a6b5ef576
skeanster <ryanskeans@gmail.com>
2023-07-21 12:49:06 -0700

    basic neighborhoods api

--- main.py
+++ main.py
@@ -21,7 +21,26 @@ class Home(Resource):
         return result
 
 
+class Neighbourhoods(Resource):
+    def get(self):
+        result = []
+
+        mycursor.execute(
+            "SELECT neighbourhood, COUNT(id) FROM sd_listings GROUP BY neighbourhood")
+
+        for x in mycursor:
+            obj = {
+                "neighbourhood": x[0],
+                "listingsCount": x[1]
+            }
+            result.append(obj)
+
+        return result
+
+
 api.add_resource(Home, "/")
+api.add_resource(Neighbourhoods, "/neighbourhoods")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
