c33218513760eb6c2ef17b84f38d7dfac3c9690c
skeanster <ryanskeans@gmail.com>
2023-07-23 18:16:54 -0700

    added users table and some user control endpoints

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ api = Api(app)
 class Home(Resource):
     def get(self):
         result = "Welcome to my Python backend!"
-        return result
+        return jsonify(result)
 
 
 class Neighbourhoods(Resource):
@@ -76,10 +76,59 @@ class ListingsByPrice(Resource):
         return jsonify(result)
 
 
+class Users(Resource):
+    def get(self):
+        result = {
+            "totalUsers": 0,
+            "users": []
+        }
+        columns = ["id", "name", "primaryNeighbourhood"]
+
+        cursor.execute("SELECT * FROM users")
+
+        for x in cursor:
+            result["users"].append((dict(zip(columns, x))))
+            result["totalUsers"] = result["totalUsers"] + 1
+
+        return jsonify(result)
+
+    def put(self):
+        neighbourhood = ''
+        request_neighbourhood = request.args.get("primaryNeighbourhood")
+        if request_neighbourhood:
+            neighbourhood = request_neighbourhood
+
+        cursor.execute(
+            "INSERT INTO users(name, primaryNeighbourhood)VALUES('"+request.args.get("name")+"','"+neighbourhood+"')")
+        db.commit()
+
+        return "User Added", 201
+
+
+class User(Resource):
+    def get(self, user_id):
+        result = []
+        columns = ["id", "name", "primaryNeighbourhood"]
+
+        cursor.execute("SELECT * FROM users WHERE id = "+user_id+";")
+
+        for x in cursor:
+            result.append(dict(zip(columns, x)))
+
+        return jsonify(result)
+
+    def delete(self, user_id):
+        cursor.execute("DELETE FROM users WHERE id = "+user_id+";")
+        db.commit()
+
+        return "user deleted", 204
+
+
 api.add_resource(Home, "/")
 api.add_resource(Neighbourhoods, "/neighbourhoods")
 api.add_resource(ListingsByPrice, "/listingsByPrice")
-
+api.add_resource(Users, "/users")
+api.add_resource(User, "/users/<string:user_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
